ff2de2270374190918c45963603443e8643d3805
Patrick_Audley <paudley@blackcatinformatics.ca>
2026-08-29 23:13:27 -0600

    fix(go): satisfy whitespace lint

--- go/cmd/coding-ethos-run/parent_workflow.go
+++ go/cmd/coding-ethos-run/parent_workflow.go
@@ -270,6 +270,7 @@ func syncParentArtifacts(
 	steps = append(steps, runParentStep("agent_hooks", func() error {
 		return agenthooks.SyncSettings(options.Repo, parentAgentHookCommand(paths))
 	}))
+
 	if !parentUsesExternalStateRoot(options) {
 		steps = append(steps, runParentStep("repo_ignores", func() error {
 			_, err := repoignore.RepairGitignore(options.Repo)
@@ -280,6 +281,7 @@ func syncParentArtifacts(
 			return nil
 		}))
 	}
+
 	steps = append(steps, runParentStep("code_intel", func() error {
 		return refreshParentCodeIntel(options.Repo)
 	}))
